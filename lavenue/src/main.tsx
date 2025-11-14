f3acbf99391e0e0a81d5ecd99acac5704af96276
Martin <lavenuexp@gmail.com>
2025-11-14 16:27:48 +0100

    added vite and noisy background

--- lavenue/src/main.tsx
+++ lavenue/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import Noise from "./components/Noise";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+      <App />
+      <Noise
+        patternSize={250}
+        patternScaleX={1}
+        patternScaleY={1}
+        patternRefreshInterval={2}
+        patternAlpha={15}
+      />
   </StrictMode>
-)
+);
