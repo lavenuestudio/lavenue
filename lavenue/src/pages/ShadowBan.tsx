8d7cfdda5ca6c2133b7f7ca931cd0e89b2d383af
Martin <lavenuexp@gmail.com>
2025-12-09 00:20:18 +0100

    optimized images and added scale animation

--- lavenue/src/pages/ShadowBan.tsx
+++ lavenue/src/pages/ShadowBan.tsx
@@ -1,12 +1,14 @@
 import "./ShadowBan.css";
 export default function ShadowBanPage() {
   return (
-    <div className="pants-wrapper">
-      <img className="patches pants-overlay-anabelle" src={`/images/parches/anabelle.png`} />
-      <img className="patches pants-overlay-shoot" src={`/images/parches/shoot.png`} />
-      <img className="patches pants-overlay-spider" src={`/images/parches/spider.png`} />
-      <img className="patches pants-overlay-lavenuePatch" src={`/images/parches/lavenuePatch.png`} />
-      <img className="pants-img" src={`/images/parches/pants.png`} />
-    </div>
+      <div className="pants-wrapper">
+        <img className="patches pants-overlay-anabelle" src={`/images/parches/anabelle.webp`} />
+        <img className="patches pants-overlay-shoot" src={`/images/parches/shoot.webp`} />
+        <img className="patches pants-overlay-spider" src={`/images/parches/spider.webp`} />
+        <img className="patches pants-overlay-lavenuePatch" src={`/images/parches/lavenuePatch.webp`} />
+        <img className="patches pants-overlay-colum" src={`/images/parches/colum.webp`} />
+        <img className="patches pants-overlay-payaso1" src={`/images/parches/payaso1.webp`} />
+        <img className="pants-img" src={`/images/parches/pants.webp`} />
+      </div>
   );
 }
