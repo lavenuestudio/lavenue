277b8af0dd0adf7229c13a8fba0089df88e6e675
Martin <lavenuexp@gmail.com>
2025-12-09 20:13:10 +0100

    added slider and new library swiper

--- lavenue/src/pages/ShadowBan.tsx
+++ lavenue/src/pages/ShadowBan.tsx
@@ -1,9 +1,15 @@
 import "./ShadowBan.css";
-import FuzzyText from "../components/FuzzyText";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
 
 export default function ShadowBanPage() {
   return (
     <div className="pants-section">
+
+    <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+    <SwiperSlide>
       <div className="pants-wrapper">
         <img className="patches pants-overlay-anabelle" src={`/images/parches/anabelle.webp`} />
         <img className="patches pants-overlay-shoot" src={`/images/parches/shoot.webp`} />
@@ -13,6 +19,30 @@ export default function ShadowBanPage() {
         <img className="patches pants-overlay-payaso1" src={`/images/parches/payaso1.webp`} />
         <img className="pants-img" src={`/images/parches/pants.webp`} />
       </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="pants-wrapper">
+          <img className="patches pants-overlay-anabelle" src={`/images/parches/anabelle.webp`} />
+          <img className="patches pants-overlay-shoot" src={`/images/parches/shoot.webp`} />
+          <img className="patches pants-overlay-spider" src={`/images/parches/spider.webp`} />
+          <img className="patches pants-overlay-lavenuePatch" src={`/images/parches/lavenuePatch.webp`} />
+          <img className="patches pants-overlay-colum" src={`/images/parches/colum.webp`} />
+          <img className="patches pants-overlay-payaso1" src={`/images/parches/payaso1.webp`} />
+          <img className="pants-img" src={`/images/parches/pants.webp`} />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="pants-wrapper">
+          <img className="patches pants-overlay-anabelle" src={`/images/parches/anabelle.webp`} />
+          <img className="patches pants-overlay-shoot" src={`/images/parches/shoot.webp`} />
+          <img className="patches pants-overlay-spider" src={`/images/parches/spider.webp`} />
+          <img className="patches pants-overlay-lavenuePatch" src={`/images/parches/lavenuePatch.webp`} />
+          <img className="patches pants-overlay-colum" src={`/images/parches/colum.webp`} />
+          <img className="patches pants-overlay-payaso1" src={`/images/parches/payaso1.webp`} />
+          <img className="pants-img" src={`/images/parches/pants.webp`} />
+        </div>
+      </SwiperSlide>
+  </Swiper>
       <div className="pants-description">
         <h2>Shadow Ban</h2>
         <p>
