5e1dc0406f36e4a5a4b4a9db87050b1b0b99d505
Martin <lavenuexp@gmail.com>
2026-01-14 16:27:15 +0100

    last

--- lavenue/src/App.tsx
+++ lavenue/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ShadowBan from "./pages/ShadowBan";
+import RunDrop from "./pages/runDrop";
 import { dbShadowBan } from "./data/db"
 import { dbShadowBanJersey } from "./data/db"
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shadowBanDrop" element={<ShadowBan shadowBanDataPants={dbShadowBan} shadowBanDataJersey={dbShadowBanJersey} /> } />
+        <Route path="/runDrop" element={<RunDrop shadowBanDataPants={dbShadowBan} shadowBanDataJersey={dbShadowBanJersey} /> } />
       </Routes>
     </>
   );
