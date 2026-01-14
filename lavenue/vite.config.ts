bbb579291c8fbb51e614321f6c2317e3026a987c
Martin <lavenuexp@gmail.com>
2026-01-14 18:41:30 +0100

    delete basic

--- lavenue/vite.config.ts
+++ lavenue/vite.config.ts
@@ -6,7 +6,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/lavenue/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
